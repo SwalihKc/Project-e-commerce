0510b300e7eba119328f30821130bf3721182325
MOHAMMED-RASIM <rashimmohmd@gmail.com>
2023-11-30 16:53:40 +0530

    ok

--- lib/review.dart
+++ lib/review.dart
@@ -24,15 +24,91 @@ class _ReviewState extends State<Review> {
             ),
           ),
         ),
-        body: Column(
-          children: [
-            Text(
-              '245 Reviews',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),Row(children: [
-             Icon(Icons.abc)
-            ],)
-          ],
+        body: Padding(
+          padding: const EdgeInsets.only(left: 15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    '245 Reviews',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                  ),
+                  SizedBox(
+                    width: 150,
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text('Add Reviews'))
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/man.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Rashim',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                  'My name is mohammed rashim, iam comming from malappuram.iam 21 years old. iam a b.com graduate.iam a flutter developer'),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Image.asset('assets/man.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Swalih',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                  'My name is mohammed rashim, iam comming from malappuram.iam 21 years old. iam a b.com graduate.iam a flutter developer'),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/man.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Hafiz',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                  'My name is mohammed rashim, iam comming from malappuram.iam 21 years old. iam a b.com graduate.iam a flutter developer'),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Image.asset('assets/man.png'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Adhu',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                  'My name is mohammed rashim, iam comming from malappuram.iam 21 years old. iam a b.com graduate.iam a flutter developer'),
+            ],
+          ),
         ),
       ),
     );
